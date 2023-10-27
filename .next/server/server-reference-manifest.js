d0b92f259fc916a36bbc29296dd90070ab729efc
luizfbarbosa12 <l.nandoferbarbosa@gmail.com>
2023-10-27 09:33:44 -0300

    feat: create auth

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ty7G811dbFONNDkEG9y22uyJdVqBDDDHgl382kgMZps=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"G5FATNeEIcvlUXZGsoEHIt1vaJIs+/rXFZTrQ9KWnqY=\"\n}"
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "45484ad4d76041e8"; }
+/******/ 		__webpack_require__.h = function() { return "eefd70b48132f50c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
